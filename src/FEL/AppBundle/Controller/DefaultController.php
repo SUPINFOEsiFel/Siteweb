34019cbef65dd312b9922b41da4972ee744563e5
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-04-12 14:22:37 +0200

    using annotation route

--- src/FEL/AppBundle/Controller/DefaultController.php
+++ src/FEL/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,27 @@
 namespace FEL\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Class DefaultController
+ * @package FEL\AppBundle\Controller
+ * @Route("/")
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="fel_app_homepage")
+     * @Template()
+     * @return array
+     */
     public function indexAction()
     {
         $em = $this->container->get('doctrine')->getEntityManager();
 
         $news = $em->getRepository('FELAppBundle:Article')->findall();
 
-
-        return $this->container->get('templating')->renderResponse('FELAppBundle:Default:index.html.twig',
-            array(
-                'news' => $news
-            ));
+        return array('news' => $news);
     }
 }
