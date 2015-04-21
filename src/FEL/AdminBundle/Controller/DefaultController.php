d77f766385b82488344c6021652365aacaed96a9
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-04-21 15:34:18 +0200

    create firewalls and controls areas, creation of authentication pages

--- src/FEL/AdminBundle/Controller/DefaultController.php
+++ src/FEL/AdminBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace FEL\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -19,6 +21,51 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        return array();
+    }
+
+    /**
+     * @Route("/login", name="fel_admin_login")
+     * @Template()
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(Request $request)
+    {
+        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        /*if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+          return $this->redirect($this->generateUrl('fel_admin_homepage'));
+        }*/
+
+        $session = $request->getSession();
+
+        // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+          $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+          $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+          $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+          'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+          'error'         => $error,
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="fel_admin_check")
+     * @Template()
+     */
+    public function checkAction(){
+        return array();
+    }
+
+    /**
+     * @Route("/logout", name="fel_admin_logout")
+     * @Template()
+     */
+    public function logoutAction(){
         return array();
     }
 }
