d872ef2ece1fc1b4c4582b5160b5b7eec97bf14f
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-04-24 19:18:49 +0200

    remove factory and authentication provider

--- src/FEL/AdminBundle/FELAdminBundle.php
+++ src/FEL/AdminBundle/FELAdminBundle.php
@@ -2,17 +2,8 @@
 
 namespace FEL\AdminBundle;
 
-use FEL\AdminBundle\DependencyInjection\Security\Factory\MeteorFactory;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FELAdminBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new MeteorFactory());
-    }
 }
