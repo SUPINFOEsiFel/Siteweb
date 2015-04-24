78fb45bc0110d57fa562bb947eb6c5b08ab0915b
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-04-24 15:05:57 +0200

    implement new authentication provider

--- src/FEL/AdminBundle/FELAdminBundle.php
+++ src/FEL/AdminBundle/FELAdminBundle.php
@@ -2,8 +2,17 @@
 
 namespace FEL\AdminBundle;
 
+use FEL\AdminBundle\DependencyInjection\Security\Factory\MeteorFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FELAdminBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new MeteorFactory());
+    }
 }
---
+++ src/FEL/AdminBundle/Security/Authentication/Token/MeteorUserToken.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FEL\AdminBundle\Security\Authentication\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+class MeteorUserToken extends AbstractToken
+{
+    public $created;
+    public $digest;
+    public $nonce;
+
+    public function __construct(array $roles = array())
+    {
+        parent::__construct($roles);
+        $this->setAuthenticated(count($roles) > 0);
+    }
+
+    public function getCredentials()
+    {
+        return '';
+    }
+}
