4a8f7f0835f67cda3c38d64b4a7d6857267ec8b2
Julien GARDET (aka. Jitrixis) <julien.gardet@jitrixis.net>
2015-05-07 21:21:40 +0200

    add events controller on the front

---
+++ src/FEL/FrontBundle/Controller/EventsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FEL\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class EventsController
+ * @package FEL\FrontBundle\Controller
+ * @Route("/events")
+ */
+class EventsController extends Controller
+{
+    /**
+     * @Route("/", name="fel_front_events")
+     * @Template()
+     */
+    public function indexAction(){
+        return array();
+    }
+}
---
+++ src/FEL/FrontBundle/Tests/Controller/EventsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FEL\FrontBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EventsControllerTest extends WebTestCase
+{
+}
